db0c809e0384360df459311eb478e2ccdf2de915
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-07-27 00:23:42 +0100

    added google maps to the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -28,6 +30,11 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        val localProperties = Properties().apply { 
+            load(project.rootProject.file("local.properties").inputStream()) 
+        }
+        manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY")
     }
 
     buildTypes {
--- lib/view/navigation/navigation_view.dart
+++ lib/view/navigation/navigation_view.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:in_porto/view/navigation/widgets/action_center.dart';
 
 class NavigationView extends StatefulWidget {
@@ -12,7 +15,18 @@ class _NavigationViewState extends State<NavigationView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       bottomNavigationBar: ActionCenter(),
+      body: GoogleMap(
+        tiltGesturesEnabled: false,
+        buildingsEnabled: false,
+        compassEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(41.14961, -8.61099),
+          zoom: 12,
+        ),
+      ),
     );
   }
 }
