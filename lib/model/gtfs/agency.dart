49a93f1808dea00cc294ad8f64ffc30883788ae7
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-07-28 18:41:55 +0100

    feat: implement Agency model with JSON serialization

---
+++ lib/model/gtfs/agency.dart
@@ -0,0 +1,45 @@
+class Agency {
+  final String name;
+  final String url;
+  final String timezone;
+  final String language;
+
+  Agency({
+    required this.name,
+    required this.url,
+    required this.timezone,
+    required this.language,
+  });
+
+  Agency copyWith({
+    String? name,
+    String? url,
+    String? timezone,
+    String? language,
+  }) {
+    return Agency(
+      name: name ?? this.name,
+      url: url ?? this.url,
+      timezone: timezone ?? this.timezone,
+      language: language ?? this.language,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'url': url,
+      'timezone': timezone,
+      'language': language,
+    };
+  }
+
+  factory Agency.fromJson(Map<String, dynamic> json) {
+    return Agency(
+      name: json['name'],
+      url: json['url'],
+      timezone: json['timezone'],
+      language: json['language'],
+    );
+  }
+}
