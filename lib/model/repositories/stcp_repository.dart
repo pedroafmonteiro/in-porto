d32b9bd965adb561995dceb896ad9462ea314917
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-02-07 20:07:02 +0000

    feat: add methods to fetch stop service ID and route schedules

--- lib/model/repositories/stcp_repository.dart
+++ lib/model/repositories/stcp_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:in_porto/model/entities/route.dart';
+import 'package:in_porto/model/entities/schedule.dart';
 import 'package:in_porto/model/entities/trip.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:in_porto/model/infrastructure/cache.dart';
@@ -41,6 +42,26 @@ class STCPRepository {
     }
   }
 
+  Future<String> fetchStopServiceId(Stop stop, DateTime? date) async {
+    final uri = Uri.parse('$_baseUrl/stops/${stop.id}/services').replace(
+      queryParameters: {
+        'date':
+            date?.toIso8601String().split('T').first ??
+            DateTime.now().toIso8601String().split('T').first,
+      },
+    );
+    final response = await _client.get(uri);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return data['active_service_id'] as String;
+    } else {
+      throw Exception(
+        'Failed to load current service for stop ${stop.id}: ${response.statusCode}',
+      );
+    }
+  }
+
   Future<List<TransportRoute>> fetchStopRoutes(Stop stop) async {
     final uri = Uri.parse('$_baseUrl/stops/${stop.id}/routes');
     final response = await _client.get(uri);
@@ -52,6 +73,60 @@ class STCPRepository {
       return results.map((json) => TransportRoute.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load stop ${stop.id}: ${response.statusCode}');
+    }
+  }
+
+  Future<List<Schedule>> fetchStopRouteSchedules(
+    Stop stop,
+    TransportRoute route,
+    String serviceId,
+  ) async {
+    final uri = Uri.parse('$_baseUrl/stops/${stop.id}/schedule').replace(
+      queryParameters: {
+        'route_id': route.id,
+        'service_id': serviceId,
+        'direction_id': route.directionId?.toString(),
+      },
+    );
+
+    final response = await _client.get(uri);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List<Schedule> schedules = [];
+
+      final dynamic scheduleData = data['schedule'];
+      final List<dynamic> hourlyGroups = [];
+      if (scheduleData is Map) {
+        hourlyGroups.addAll(scheduleData.values);
+      } else if (scheduleData is List) {
+        hourlyGroups.addAll(scheduleData);
+      }
+
+      for (final arrivalsByHour in hourlyGroups) {
+        if (arrivalsByHour is List) {
+          for (final arrivalData in arrivalsByHour) {
+            if (arrivalData is Map<String, dynamic> &&
+                arrivalData['arrival_time'] != null) {
+              schedules.add(
+                Schedule.fromJson({
+                  'stop_id': stop.id,
+                  'route_id': route.id,
+                  'direction_id': route.directionId?.toString(),
+                  'service_id': serviceId,
+                  'arrival_time': arrivalData['arrival_time'],
+                }),
+              );
+            }
+          }
+        }
+      }
+
+      return schedules;
+    } else {
+      throw Exception(
+        'Failed to load schedules for stop ${stop.id}: ${response.statusCode}',
+      );
     }
   }
 
