43e2ecebf07fc31a9fe1edaffa65b7325e650c03
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-02-07 18:55:42 +0000

    feat: created schedule model

---
+++ lib/model/entities/schedule.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'schedule.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Schedule _$ScheduleFromJson(Map<String, dynamic> json) => Schedule(
+  stopId: json['stop_id'] as String,
+  routeId: json['route_id'] as String,
+  directionId: json['direction_id'] as String?,
+  serviceId: json['service_id'] as String,
+  arrivalTime: json['arrival_time'] as String,
+);
+
+Map<String, dynamic> _$ScheduleToJson(Schedule instance) => <String, dynamic>{
+  'stop_id': instance.stopId,
+  'route_id': instance.routeId,
+  'direction_id': instance.directionId,
+  'service_id': instance.serviceId,
+  'arrival_time': instance.arrivalTime,
+};
