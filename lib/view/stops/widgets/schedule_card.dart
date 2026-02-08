d96dc04fba5bf8e18ee9674f2012401f63aa398f
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-02-08 22:47:00 +0000

    feat: added new card to show departure times from stop

---
+++ lib/view/stops/widgets/schedule_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:in_porto/l10n/app_localizations.dart';
+import 'package:in_porto/model/entities/route.dart';
+import 'package:in_porto/model/entities/schedule.dart';
+import 'package:in_porto/view/common/route_badge.dart';
+import 'package:in_porto/view/stops/utils/stop_utils.dart';
+
+class ScheduleCard extends StatelessWidget {
+  const ScheduleCard({
+    super.key,
+    required this.schedule,
+    required this.route,
+    this.isToday = true,
+  });
+
+  final Schedule schedule;
+  final TransportRoute? route;
+  final bool isToday;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0.1,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: ListTile(
+        leading: RouteBadge(
+          number: route!.shortName,
+          color: route!.color,
+          textColor: route!.textColor,
+        ),
+        title: Text(
+          route!.tripHeadsign ?? 'Unknown Route',
+          style: Theme.of(
+            context,
+          ).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              StopUtils.formatArrivalTime(
+                schedule.arrivalTime,
+                isToday: isToday,
+                context: context,
+              ),
+              style: Theme.of(
+                context,
+              ).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text(AppLocalizations.of(context)!.scheduled),
+          ],
+        ),
+      ),
+    );
+  }
+}
